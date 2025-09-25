12578c73109e6397fc6aae328c3172b860e3ba82
Oleksandr Kurzhy <ahilles.km@gmail.com>
2025-09-25 10:16:52 +0300

    Update project configuration and home route metadata
    - Added .idea to .dockerignore and .gitignore
    - Updated home route metadata to reflect the app's name and description
    - Added new dependencies: @tanstack/react-query and lucide-react

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -3,8 +3,8 @@ import { Welcome } from "../welcome/welcome";
 
 export function meta({}: Route.MetaArgs) {
   return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
+    { title: "My Gifs App" },
+    { name: "description", content: "Welcome to My Gifs App!" },
   ];
 }
 
